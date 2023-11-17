b057b2e2155faf43c2c48be55dfc40ba85625878
Herman Haugen Mo <hermanhm@stud.ntnu.no>
2023-11-17 16:09:17 +0100

    added more comments

--- main.py
+++ main.py
@@ -4,68 +4,68 @@ import os
 
 app = Flask(__name__)
 
-# Load the model and the transformer
+# Load the ML model and the pre-processing transformer from the 'models' directory
 model = joblib.load('models/polynomial_regression_meal_model.joblib')
 poly_transformer = joblib.load('models/poly_transformer.joblib')
 
-
 @app.route('/')
 def index():
+    # Render the main page of the web app
     return render_template('index.html')
-
 
 @app.route('/calculate_score', methods=['POST'])
 def calculate_score():
+    # Retrieve data from the POST request
     data = request.get_json()
 
-    # Ensure all fields are present
+    # Check if all required fields (cost, protein, calories) are in the data
     if 'cost' not in data or 'protein' not in data or 'calories' not in data:
         return jsonify({'error': 'Missing data'}), 400
 
     try:
-        # Convert to float and validate
+        # Attempt to convert the inputs to float and validate if they are non-negative
         cost = float(data['cost'])
         protein = float(data['protein'])
         calories = float(data['calories'])
 
         if cost < 0 or protein < 0 or calories < 0:
+            # Raise a ValueError if any input is negative
             raise ValueError
 
     except (ValueError, TypeError):
+        # Handle any type conversion errors or negative values
         return jsonify({'error': 'Invalid input'}), 400
 
-    # Create the features array
+    # Prepare the input features for the ML model
     features = [cost, protein, calories]
 
-    # Transform the features into polynomial features
+    # Apply polynomial transformation to the features
     poly_features = poly_transformer.transform([features])
 
-    # Predict the score
+    # Use the ML model to predict the score
     score = model.predict(poly_features)[0]
 
-    # Round the score to 2 decimal places
+    # Round the predicted score to 2 decimal places
     rounded_score = round(score, 2)
 
-    # Cap the score at 10 and return
+    # Ensure the score is capped between 0 and 10
     if rounded_score > 10:
         return jsonify({'score': 10.00})
     if rounded_score < 0:
         return jsonify({'score': 0.00})
     return jsonify({'score': rounded_score})
 
-
 @app.errorhandler(500)
 def internal_error(error):
+    # Custom error handler for internal server errors
     return jsonify({'error': 'Internal Server Error'}), 500
-
 
 @app.errorhandler(404)
 def not_found_error(error):
+    # Custom error handler for "not found" errors
     return jsonify({'error': 'Resource Not Found'}), 404
 
-
 if __name__ == '__main__':
-    # Get the port number from the environment variable
-    port = int(os.environ.get('PORT', 8000))  # Use 8080 as a default if $PORT is not set
-    # Run the app
+    # Start the Flask app on the specified port
+    port = int(os.environ.get('PORT', 8000))  # Default to 8080 if $PORT not set
     app.run(host='0.0.0.0', port=port)
--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const submitButton = document.getElementById('submit');
     const inputs = document.querySelectorAll('input[type="number"]');
 
-    // Enable button when all fields are filled
+    // Check if all input fields are filled to enable the submit button
     inputs.forEach(input => {
         input.addEventListener('input', () => {
             let allFilled = Array.from(inputs).every(input => input.value.length > 0);
@@ -10,40 +10,41 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Event listener for the submit button
+    // Handling the click event on the submit button
     submitButton.addEventListener('click', function() {
+        // Retrieve and parse input values
         let cost = parseFloat(document.getElementById('cost').value);
         let protein = parseFloat(document.getElementById('protein').value);
         let calories = parseFloat(document.getElementById('calories').value);
 
-        // Basic validation checks
+        // Perform basic validation on the inputs
         if (isNaN(cost) || cost < 0 || isNaN(protein) || protein < 0 || isNaN(calories) || calories < 0) {
+            // If validation fails, exit the function without proceeding
             return;
         }
 
-        // Update or transform these variables as required by your model
+        // Prepare the data object for the API request
         let features = {
-            cost: parseFloat(cost),
-            protein: parseFloat(protein),
-            calories: parseFloat(calories)
+            cost: cost,
+            protein: protein,
+            calories: calories
         };
 
+        // Sending a POST request to the '/calculate_score' endpoint
         fetch('/calculate_score', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            cost: parseFloat(cost),
-            protein: parseFloat(protein),
-            calories: parseFloat(calories)
-        }),
-    })
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(features),
+        })
         .then(response => response.json())
         .then(data => {
+            // Update the score element with the received score
             let scoreElement = document.getElementById('score');
             scoreElement.innerText = `Score: ${data.score}`;
-            scoreElement.classList.remove('score-placeholder'); // Remove placeholder styling
+            // Remove the placeholder styling after receiving the score
+            scoreElement.classList.remove('score-placeholder');
         });
     });
 });
